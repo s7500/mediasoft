91b1238d44f97fc7135d7d2ca3e80513514268a4
s7500 <stezurrodion69@gmail.com>
2021-03-08 20:53:26 +0300

    v. 1.0

--- catologue/views.py
+++ catologue/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+import django_filters.rest_framework
+from rest_framework import viewsets, generics
 from .serializers import CategorySerializer
 from .serializers import SubcategorySerializer
 from .serializers import ProductSerializer
@@ -7,34 +8,27 @@ from .models import Subcategory
 from .models import Product
 
 
-class ProductViewSet(viewsets.ModelViewSet):
+class ProductList(generics.ListAPIView):
     serializer_class = ProductSerializer
     filterset_fields = '__all__'
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
 
     def get_queryset(self):
         queryset = Product.objects.all()
-        return queryset
-    # def get_queryset(self, *args, **kwargs):
-    #     queryset = Product.objects.all()
-    #     feature = self.kwargs['feature']
-    #     value = self.kwargs['value']
-    #     if feature and value:
-    #         queryset = Product.objects.filter(
-    #                     productfields__feature=feature,
-    #                     productfields__value=value
-    #                 )
+        feature = self.kwargs['feature']
+        value = self.kwargs['value']
+        print(feature, value)
+        if feature != None and value != None:
+            queryset = Product.objects.filter(
+                        productfields__feature=feature,
+                        productfields__value=value
+                    )
 
-    #     return queryset
+        return queryset
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-    filterset_fields = '__all__'
-
-
-class ViewSet(viewsets.ModelViewSet):
-    queryset = Subcategory.objects.all()
     serializer_class = CategorySerializer
     filterset_fields = '__all__'
     
--- mediasoft/urls.py
+++ mediasoft/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from catologue import views
 from rest_framework import routers
 
 
 router = routers.DefaultRouter()
 router.register(r'categories', views.CategoryViewSet)
-router.register(r'products', views.ProductViewSet, basename='products',)
+# router.register(r'products', views.ProductList, basename='products',)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    re_path(r'^api/products/((?P<feature>.+)\=(?P<value>.+))/$', views.ProductList.as_view(),)
 ]
